4ae0ef0e1f2e36885a0b5d52b87893e29ce84fae
Dias <wprogrammist@gmail.com>
2022-12-14 10:22:13 +0600

    sss

--- sctipt.js
+++ sctipt.js
@@ -3,3 +3,28 @@ $(document).ready(function(){
         // setting-name: setting-value
     });
 });
+
+const modal = document.querySelector('#modal');
+document.body.addEventListener('click', function(e){
+    modal.classList.remove('active');
+    console.log("It works")
+})
+
+const playBtns = document.querySelectorAll('.people__play');
+console.log('playBtns :', playBtns);
+
+for(let i=0; i<playBtns.length; i++){
+    playBtns[i].addEventListener('click', function(e){
+        e.stopPropagation();
+        console.log("It works")
+        modal.classList.add('active');
+        console.log('modal :', modal);
+        
+    })
+}
+
+// playBtns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+        
+//     })
+// })
